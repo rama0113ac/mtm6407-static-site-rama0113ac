257df70b2b9e4be4292511664fac3ebbde648f0b
rama0113ac <136387562+rama0113ac@users.noreply.github.com>
2024-11-20 04:38:22 +0500

    Delete dist directory

--- dist/mtm6407-static-site/_nuxt/static/1732029936/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002FCookingTips","\u002FRecipes","\u002FAbout","\u002F","\u002FContact","\u002Frecipes","\u002Fcookingtips","\u002Fabout","\u002Fcontact"]})
