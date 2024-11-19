83cba23045eab2fded95206e797b05a5b3d04758
rama0113ac <136387562+rama0113ac@users.noreply.github.com>
2024-11-20 00:31:03 +0500

    dist complete

---
+++ dist/_nuxt/static/1732015152/Recipes/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fflavors-of-home\u002F_nuxt\u002Fstatic\u002F1732015152",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Frecipes",config:{_app:{basePath:"\u002Fflavors-of-home\u002F",assetsPath:"\u002Fflavors-of-home\u002F_nuxt\u002F",cdnURL:a}}}}(null));
