ea533d77658e0706fd146e44d9507ca08ca1205b
rama0113ac <136387562+rama0113ac@users.noreply.github.com>
2024-11-20 01:00:37 +0500

    Delete dist directory

--- dist/_nuxt/static/1732015152/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FContact","\u002FAbout","\u002FCookingTips","\u002FRecipes","\u002Frecipes","\u002Fabout","\u002Fcookingtips","\u002Fcontact"]})
