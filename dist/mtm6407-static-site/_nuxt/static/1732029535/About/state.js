c56c6a6260aee44dc9ce44550e15005cbe13f2c3
rama0113ac <136387562+rama0113ac@users.noreply.github.com>
2024-11-20 04:28:13 +0500

    Complete dist

---
+++ dist/mtm6407-static-site/_nuxt/static/1732029535/About/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmtm6407-static-site\u002Fmtm6407-static-site\u002F_nuxt\u002Fstatic\u002F1732029535",layout:"default",error:a,state:{},serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fmtm6407-static-site\u002F",assetsPath:"\u002Fmtm6407-static-site\u002Fmtm6407-static-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));
