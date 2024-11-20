2768c70995b38cc9d5ef1c457e7a43a1284f07f0
rama0113ac <136387562+rama0113ac@users.noreply.github.com>
2024-11-20 05:13:55 +0500

    changes

---
+++ _nuxt/static/1732032171/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FContact","\u002FRecipes","\u002FCookingTips","\u002FAbout","\u002Frecipes","\u002Fcookingtips","\u002Fabout","\u002Fcontact"]})
