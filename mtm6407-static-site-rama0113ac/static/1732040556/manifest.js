fb380fa000522e185c5ebc3b35dfe93b1627b3e3
rama0113ac <136387562+rama0113ac@users.noreply.github.com>
2024-11-20 07:47:01 +0500

    files upload

---
+++ mtm6407-static-site-rama0113ac/static/1732040556/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FRecipes","\u002FCookingTips","\u002FContact","\u002FAbout","\u002Fabout","\u002Frecipes","\u002Fcontact","\u002Fcookingtips"]})
